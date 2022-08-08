207d7ccbc2ac6528e535f3f909468696fd6aa532
ViMuilu <vmuilu@gmail.com>
2022-08-08 12:14:01 +0300

    added basic queries

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 
+
 application = Flask(__name__)
 application.config["MONGO_URI"] = ("mongodb://"
     + os.environ["MONGO_INITDB_ROOT_USERNAME"]
@@ -67,17 +68,76 @@ def getItem():
     data.append({"datasets": datasets})   
     data.append({"images": images})
     data.append({"samples": samples})
-
-
     return jsonify(status=True, data=data)
 
 
 @application.route("/query", methods=["POST"])
 def searchQueary():
+
     """Search query."""
-    return jsonify(), 201
+    # Get sample info  
+    dbSamples = getSamples(request)
+    if not dbSamples:
+        return jsonify(error="No results found.")
+    images = getImages(dbSamples)
+    return jsonify(results=str(images)), 201
 
+def getSamples(request):
+    dbSamples = []
+    requestBiological = request.get_json().get('biologicalBeing')
+    requestAnatomical = request.get_json().get('anatomicalSite')
+    requestSex = request.get_json().get('sex')
+    requestAge = request.get_json().get('age')
+    if(request.get_json().get("ageCondition") == "<"):
+        # Age less than
+        dbSamples.append(list(db.sample.find({'specimen.attributes.attribute': {"$elemMatch": 
+        { "tag": "age_at_extraction", "value": { "$lt": requestAge}, "tag": "anatomical_site", "value": requestAnatomical}}})))
 
+    elif(request.get_json().get("ageCondition") == ">"):
+        # Age more than
+        dbSamples.append(list(db.sample.find({'specimen.attributes.attribute': {"$elemMatch": 
+        { "tag": "age_at_extraction", "value": { "$gt": requestAge}, "tag": "anatomical_site", "value": requestAnatomical}}})))
+    elif(request.get_json().get("ageCondition") == "-"):
+        # Ages between       
+        dbSamples.append(list(db.sample.find({'specimen.attributes.attribute': {"$elemMatch": 
+        { "tag": "age_at_extraction", "value": { "$gte": request.form.get('ageStart'), "$lt": request.form.get('ageEnd')},
+         "tag": "anatomical_site", "value": requestAnatomical}}})))
+    else:
+        # No age
+        dbSamples.append(list(db.sample.find({"biologicalBeing.alias": requestBiological, 'biologicalBeing.attributes.attribute.value': requestSex})))
+        dbSamples.append(list(db.sample.find({"specimen.attributes.attribute.value": requestAnatomical})))
+
+    return dbSamples
+def getImages(dbSamples):
+    images= []
+    for sample in dbSamples:
+        print('\x1b[6;30;42m' + str(sample) + '\x1b[0m')
+        keys = sample[0].keys()
+        for key in keys:
+            if(key == "biologicalBeing"):
+                images.append(getImageByBiologicalBeing(sample[0].get("biologicalBeing")))
+            elif (key == "specimen"):
+                images.append(getImageBySpecimen(sample[0].get("specimen")))
+    return images
+
+def getImageByBiologicalBeing(biologicalBeing):
+
+    specimenOfBiologicalBeing = list(db.sample.find({"specimen.extractedFrom.refname": biologicalBeing.get("alias")}))
+
+    blockOfSpecimen =  list(db.sample.find({'block.sampledFrom.refname': specimenOfBiologicalBeing[0].get("specimen").get("alias")}))
+
+    slideOfBlock = list(db.sample.find({'slide.createdFrom.refname': blockOfSpecimen[0].get("block").get("alias")}))
+
+    return list(db.images.find({"imageOf.refname": slideOfBlock[0].get("slide").get("alias")}))
+
+def getImageBySpecimen(specimen):
+    blockOfSpecimen =  list(db.sample.find({'block.sampledFrom.refname': specimen.get("alias")}))
+
+    slideOfBlock = list(db.sample.find({'slide.createdFrom.refname': blockOfSpecimen[0].get("block").get("alias")}))
+
+    return list(db.images.find({"imageOf.refname": slideOfBlock[0].get("slide").get("alias")}))
+
+    
 if __name__ == "__main__":
     application.config.from_prefixed_env()
     application.config["APP_PORT"]
