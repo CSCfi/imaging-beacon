5c9af9d5b2f5280551cad90cecef6f435a124eac
ViMuilu <vmuilu@gmail.com>
2022-08-29 13:13:38 +0300

    fixed anatomical search

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 import json
+import bson.json_util as json_util
 
 application = Flask(__name__)
 application.config["MONGO_URI"] = ("mongodb://"
@@ -29,14 +30,15 @@ def getItem():
     return jsonify(response=getItems(db)), 200
 
 @application.route("/getSearchTerms")
-def getSearchTerms():
+def returnearchTerms():
    return jsonify(response=getSearchTerms(db)), 200
 
 @application.route("/query", methods=["POST"])
 def query():
     """Search query.""" 
     result = searchQuery(request, db)
-    return jsonify(response=str(result)), 200
+    
+    return json.loads(json_util.dumps(result)), 200
     
 if __name__ == "__main__":
     application.config.from_prefixed_env()
